0ddfae756389e9a9d5f7285e662c0008fe2ab337
Sasindu Fernando <pujana.sasindu@gmail.com>
2024-07-09 17:21:19 +0530

    create booking page

---
+++ src/pages/admin_page.jsx
@@ -0,0 +1,38 @@
+
+
+import React from 'react';
+
+const AdminPage = () => {
+  
+
+  return (
+    <div>
+      <h1>Admin Page</h1>
+     
+      <table>
+        <thead>
+          <tr>
+            <th>Booking Date</th>
+            <th>User Name</th>
+            <th>Vehicle Number</th>
+            <th>Phone Number</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          
+          <tr>
+            <td>2024-07-10</td>
+            <td>John Doe</td>
+            <td>ABC1234</td>
+            <td>9876543210</td>
+            
+          </tr>
+          
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default AdminPage;
